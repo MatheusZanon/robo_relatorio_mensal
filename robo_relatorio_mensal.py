58dc3277527331685705c64d3009e27e3c487210
MatheusZanon <mzblannes@outlook.com>
2024-06-26 16:54:09 -0300

    feat: criando pasta templates para arquivos externos

--- robo_relatorio_mensal.py
+++ robo_relatorio_mensal.py
@@ -328,7 +328,7 @@ class execute(Resource):
     dir_relatorio_926 = f"{particao}:\\Meu Drive\\Relatorio_Human_9.26_Direitos_Trabalhistas\\{ano}\\Relatório {ano} Human - 9,26_ Direitos Trabalhistas.xlsx"
     dir_relatorio_taxa_adm = f"{particao}:\\Meu Drive\\Relatorio_Taxa_Administracao\\{ano}\\Taxa Administração {ano} Human.xlsx"
     dir_relatorio_economia_manaus = f"{particao}:\\Meu Drive\\Relatorio_Economia_Mensal_Manaus\\{ano}\\Relatorio Economia Mensal Manaus {ano}.xlsx"
-    dir_dentistas_norte_modelo = Path(f"{particao}:\\Meu Drive\\Arquivos_Automacao\\Dentistas_Norte_Modelo_00_0000_python.xlsx")
+    dir_dentistas_norte_modelo = Path(f"{particao}:\\Meu Drive\\Arquivos_Automacao\\Dentistas_Norte_Modelo_0000_00_python.xlsx")
     dir_dentistas_norte_destino = Path(f"{particao}:\\Meu Drive\\Relatorio_Dentista_do_Norte\\{mes}-{ano}")
     dir_economia_mensal_modelo = Path(f"{particao}:\\Meu Drive\\Arquivos_Automacao\\modelo relatorio demonstrativo economia previdencia.xlsx")
     mes_nome = calendar.month_name[int(mes)].capitalize()
